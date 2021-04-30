660a02f5f895689303dac52da659478d9b8a58f4
tom_glb <ppp_glb@qq.com>
2021-04-30 09:44:54 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,8 +1,9 @@
 package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git version 1");
+        System.out.println("git version1");
         System.out.println("git version2");
+        System.out.println("git version3");
     }
 
 }
