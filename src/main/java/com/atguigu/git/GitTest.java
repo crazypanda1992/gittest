479293e76cad3c1d339922ea4b928d8c27db918e
tom_glb <ppp_glb@qq.com>
2021-04-30 09:43:59 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git version 1");
+        System.out.println("git version2");
     }
 
 }
